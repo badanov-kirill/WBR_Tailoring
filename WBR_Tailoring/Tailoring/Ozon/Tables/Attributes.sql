d7cfe6b93dca7741dc3400aa83b256ebb8e85c72
Баданов Кирилл <kirill.badanov@bk.ru>
2021-11-13 16:02:56 +0300

    dev

--- WBR_Tailoring/Tailoring/Ozon/Tables/Attributes.sql
+++ WBR_Tailoring/Tailoring/Ozon/Tables/Attributes.sql
@@ -2,5 +2,10 @@
 (
 	attribute_id INT CONSTRAINT [PK_Attributes] PRIMARY KEY CLUSTERED NOT NULL,
 	attribute_name VARCHAR(50) NOT NULL,
-	attribute_descr VARCHAR(500) NOT NULL
+	attribute_descr VARCHAR(500) NOT NULL,
+	data_type_id SMALLINT CONSTRAINT [FK_OzonAttributes_data_type_id] FOREIGN KEY REFERENCES dbo.DataTypes(data_type_id) NULL,
+	is_collection BIT NULL,
+	dictionary_id INT NULL,
+	group_id INT NULL,
+	group_name VARCHAR(25) NULL,
 )
---
+++ WBR_Tailoring/Tailoring/dbo/Tables/DataTypes.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[DataTypes]
+(
+	data_type_id       SMALLINT IDENTITY(1, 1) CONSTRAINT [PK_DataTypes] PRIMARY KEY CLUSTERED NOT NULL,
+	data_type_name     VARCHAR(100) NOT NULL
+)
