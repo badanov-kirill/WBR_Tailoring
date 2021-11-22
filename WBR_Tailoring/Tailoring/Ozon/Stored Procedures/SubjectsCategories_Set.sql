01ffb9a55c1bbfedf127e281311e88860f6ad9ba
Баданов Кирилл <kirill.badanov@bk.ru>
2021-11-22 23:47:36 +0300

    dev

---
+++ WBR_Tailoring/Tailoring/Ozon/Stored Procedures/SubjectsCategories_Set.sql
@@ -0,0 +1,43 @@
+﻿CREATE PROCEDURE [Ozon].[SubjectsCategories_Set]
+	@data Ozon.SubjectsCategoriesType READONLY,
+	@employee_id INT
+AS
+	SET NOCOUNT ON
+	SET XACT_ABORT ON
+	DECLARE @dt DATETIME2(0) = GETDATE()
+	
+	BEGIN TRY
+		BEGIN TRANSACTION
+		
+		DELETE	
+		FROM	Ozon.SubjectsCategories
+		
+		INSERT INTO Ozon.SubjectsCategories
+			(
+				subject_id,
+				category_id,
+				dt,
+				employee_id
+			)
+		SELECT	d.subject_id,
+				d.category_id,
+				@dt,
+				@employee_id
+		FROM	@data d
+		
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0
+		    ROLLBACK TRANSACTION
+		
+		DECLARE @ErrNum INT = ERROR_NUMBER();
+		DECLARE @estate INT = ERROR_STATE();
+		DECLARE @esev INT = ERROR_SEVERITY();
+		DECLARE @Line INT = ERROR_LINE();
+		DECLARE @Mess VARCHAR(MAX) = CHAR(10) + ISNULL('Процедура: ' + ERROR_PROCEDURE(), '') 
+		        + CHAR(10) + ERROR_MESSAGE();
+		
+		RAISERROR('Ошибка %d в строке %d  %s', @esev, @estate, @ErrNum, @Line, @Mess) 
+		WITH LOG;
+	END CATCH 
