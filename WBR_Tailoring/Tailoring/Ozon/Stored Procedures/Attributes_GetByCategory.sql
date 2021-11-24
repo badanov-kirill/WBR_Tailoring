99fcc64be57ddac6ddf000f50343f4ad15ca91a3
Баданов Кирилл <kirill.badanov@bk.ru>
2021-11-24 23:23:50 +0300

    dev

---
+++ WBR_Tailoring/Tailoring/Ozon/Stored Procedures/Attributes_GetByCategory.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [Ozon].[Attributes_GetByCategory]
+	@category_id BIGINT
+AS
+	SET NOCOUNT ON
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	
+	SELECT	a.attribute_id,
+			a.attribute_name,
+			a.attribute_descr,
+			ca.is_required,
+			dt.data_type_name
+	FROM	Ozon.CategoriesAttributes ca   
+			INNER JOIN	Ozon.Attributes a
+				ON	a.attribute_id = ca.attribute_id   
+			INNER JOIN	dbo.DataTypes dt
+				ON	dt.data_type_id = a.data_type_id
+	WHERE	ca.category_id = @category_id
+	
+	SELECT	ca.attribute_id,
+			av.av_id,
+			av.av_value
+	FROM	Ozon.CategoriesAttributes ca   
+			INNER JOIN	Ozon.CategoriesAttributeValues cav
+				ON	cav.category_id = ca.category_id
+				AND	cav.attribute_id = ca.attribute_id   
+			INNER JOIN	Ozon.AttributeValues av
+				ON	av.av_id = cav.av_id
+	WHERE	ca.category_id = @category_id
---
+++ WBR_Tailoring/Tailoring/Products/Stored Procedures/AddedOption_GetBySubject.sql
@@ -0,0 +1,93 @@
+﻿CREATE PROCEDURE [Products].[AddedOption_GetBySubject]
+	@subject_id INT
+AS
+	SET NOCOUNT ON
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	
+	DECLARE @carething_tab TABLE(ao_id INT)
+	INSERT INTO @carething_tab
+	  (
+	    ao_id
+	  )
+	SELECT	ctao.ao_id
+	FROM	Products.CareThingAddedOption ctao
+	UNION
+	SELECT	ao.ao_id_parent
+	FROM	Products.CareThingAddedOption ctao   
+			INNER JOIN	Products.AddedOption ao
+				ON	ao.ao_id = ctao.ao_id
+	;
+	WITH cte_ao AS
+	(
+		SELECT	ao.ao_id,
+				ao.ao_name,
+				ao.ao_id_parent,
+				ao.ao_name_eng,
+				ao.is_bool,
+				ao.si_id,
+				ao.ao_type_id,
+				1                   lvl,
+				sao.required_mode
+		FROM	Products.SubjectAddedOption sao   
+				INNER JOIN	Products.AddedOption ao
+					ON	ao.ao_id = sao.ao_id   
+				LEFT JOIN	@carething_tab ct
+					ON	ct.ao_id = ao.ao_id
+		WHERE	sao.subject_id = @subject_id
+				AND	ct.ao_id IS     NULL
+				AND ao.isdeleted = 0
+				AND ao.content_id IS NOT NULL
+				AND (ao.ao_id_parent IS NULL OR ao.ao_id_parent != 7)
+	)
+	,
+	cte_parent AS (
+		SELECT	c.ao_id,
+				c.ao_name,
+				c.ao_id_parent,
+				c.ao_name_eng,
+				c.is_bool,
+				c.si_id,
+				c.ao_type_id,
+				c.lvl,
+				c.required_mode
+		FROM	cte_ao c
+		UNION ALL
+		SELECT	ao.ao_id,
+				ao.ao_name,
+				ao.ao_id_parent,
+				ao.ao_name_eng,
+				ao.is_bool,
+				ao.si_id,
+				ao.ao_type_id,
+				c.lvl - 1,
+				NULL
+		FROM	cte_parent c   
+				INNER JOIN	Products.AddedOption ao
+					ON	ao.ao_id = c.ao_id_parent
+		WHERE	NOT EXISTS(
+		     		SELECT	1
+		     		FROM	cte_ao cao
+		     		WHERE	cao.ao_id = ao.ao_id
+					)
+					AND ao.isdeleted = 0
+	)
+	
+	SELECT	DISTINCT ao.ao_id,
+			ao.ao_id_parent,
+			ao.ao_name,
+			ao.ao_name_eng,
+			ao.is_bool,
+			ao.si_id,
+			s.si_name,
+			aot.ao_type_name,
+			ao.ao_type_id,
+			ao.required_mode,
+			ao.lvl
+	FROM	cte_parent ao			 
+			LEFT JOIN	Products.SI s
+				ON	s.si_id = ao.si_id  
+			LEFT JOIN	Products.AddedOptionType aot
+				ON	aot.ao_type_id = ao.ao_type_id
+	ORDER BY
+		ao.lvl,
+		ao.ao_name
