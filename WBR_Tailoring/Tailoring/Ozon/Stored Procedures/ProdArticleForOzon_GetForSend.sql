6a8f5513f401bf7dfbe94401a8338cb82a8f70d4
Баданов Кирилл <kirill.badanov@bk.ru>
2021-12-17 12:39:08 +0300

    dev

---
+++ WBR_Tailoring/Tailoring/Ozon/Stored Procedures/ProdArticleForOzon_GetForSend.sql
@@ -0,0 +1,32 @@
+﻿CREATE PROCEDURE [Ozon].[ProdArticleForOzon_GetForSend]
+AS
+	SET NOCOUNT ON
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	
+	SELECT	pafw.pa_id,
+			CAST(pafw.dt AS DATETIME)     dt,
+			b.brand_name,
+			sj.subject_name,
+			an.art_name,
+			pa.sa,
+			oasa.x                        sa_nms,
+			pa.imt_id
+	FROM	Wildberries.ProdArticleForWB pafw   
+			INNER JOIN	Products.ProdArticle pa
+				ON	pa.pa_id = pafw.pa_id   
+			INNER JOIN	Products.Sketch s
+				ON	s.sketch_id = pa.sketch_id   
+			INNER JOIN	Products.Brand b
+				ON	b.brand_id = pa.brand_id   
+			INNER JOIN	Products.ArtName an
+				ON	an.art_name_id = s.art_name_id   
+			INNER JOIN	Products.[Subject] sj
+				ON	sj.subject_id = s.subject_id   
+			OUTER APPLY (
+			      	SELECT	pan.sa + ';'
+			      	FROM	Products.ProdArticleNomenclature pan
+			      	WHERE	pan.pa_id = pa.pa_id
+			      	FOR XML	PATH('')
+			      ) oasa(x)
+	WHERE	pafw.send_dt IS NULL
+			--AND	ISNULL(pafw.is_error, 0) = 0 
