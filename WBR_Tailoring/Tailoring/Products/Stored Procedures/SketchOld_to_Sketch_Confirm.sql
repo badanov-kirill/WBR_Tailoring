b60541d27c8b15d20d8cdb90c888e36e732c3c4a
Баданов Кирилл <kirill.badanov@bk.ru>
2022-08-24 21:17:31 +0300

    dev

--- WBR_Tailoring/Tailoring/Products/Stored Procedures/SketchOld_to_Sketch_Confirm.sql
+++ WBR_Tailoring/Tailoring/Products/Stored Procedures/SketchOld_to_Sketch_Confirm.sql
@@ -11,11 +11,11 @@ AS
 	SELECT	@error_text = CASE 
 	      	                   WHEN so.so_id IS NULL THEN 'Архивного эскиза с кодом ' + CAST(v.so_id AS VARCHAR(10)) + ' не существует'
 	      	                   WHEN s.sketch_id IS NULL THEN 'Артикула с кодом ' + CAST(v.sketch_id AS VARCHAR(10)) + ' не существует'
-	      	                   WHEN so.brand_id IS NULL OR so.st_id IS NULL OR so.subject_id IS NULL OR so.season_id IS NULL OR so.model_year IS NULL OR so.sa_local 
-	      	                        IS NULL OR so.model_number IS NULL OR so.art_name IS NULL OR so.brand_id != s.brand_id OR so.model_year != s.art_year OR 
-	      	                        so.st_id != s.st_id OR so.subject_id != s.subject_id OR so.season_id != s.season_id OR so.sa_local != s.sa_local OR so.model_number 
-	      	                        != s.model_number
-	      	                        OR s.art_name_id != an.art_name_id THEN 'Не совпадают данные у старого и нового артикула, невозможно завершить перенос'
+	      	                   --WHEN so.brand_id IS NULL OR so.st_id IS NULL OR so.subject_id IS NULL OR so.season_id IS NULL OR so.model_year IS NULL OR so.sa_local 
+	      	                   --     IS NULL OR so.model_number IS NULL OR so.art_name IS NULL OR so.brand_id != s.brand_id OR so.model_year != s.art_year OR 
+	      	                   --     so.st_id != s.st_id OR so.subject_id != s.subject_id OR so.season_id != s.season_id OR so.sa_local != s.sa_local OR so.model_number 
+	      	                   --     != s.model_number
+	      	                   --     OR s.art_name_id != an.art_name_id THEN 'Не совпадают данные у старого и нового артикула, невозможно завершить перенос'
 	      	                   ELSE NULL
 	      	              END
 	FROM	(VALUES(@sketch_id,
