c8a1204a9600d76c1c696d9de7174e924a631d67
Баданов Кирилл <kirill.badanov@bk.ru>
2022-11-24 22:47:19 +0300

    dev

---
+++ REP_Tailoring/REP_Tailoring/Sale/Stored Procedures/MonthRedortDetail_GetByID_KZ.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [Sale].[MonthRedortDetail_GetByID_KZ]
+	@realizationreport_id INT
+AS
+	SET NOCOUNT ON
+	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+	
+	SELECT	sa.sa_name,
+			''                    ts_name,
+			SUM(mrd.quantity)     quantity,
+			mrd.nds,
+			SUM(mrd.ppvz_for_pay - mrd.delivery_rub) retail_amount,
+			CAST(MAX(mrd.sale_dt) AS DATETIME)
+			sale_dt,
+			CAST(MAX(mrd.sale_dt) AS DATETIME) sale_period_dt
+	FROM	Sale.MonthReportDetail mrd   
+			INNER JOIN	Sale.MonthReport mr
+				ON	mr.realizationreport_id = mrd.realizationreport_id   
+			INNER JOIN	Products.SupplierArticle sa
+				ON	sa.sa_id = mrd.sa_id   
+			INNER JOIN	Products.TechSize ts
+				ON	ts.ts_id = mrd.ts_id
+	WHERE	mrd.doc_type_id = 1
+			AND	mrd.realizationreport_id = @realizationreport_id
+	GROUP BY
+		sa.sa_name,
+		mrd.nds
+	HAVING
+		SUM(mrd.quantity) > 0
+	
+	
+	
