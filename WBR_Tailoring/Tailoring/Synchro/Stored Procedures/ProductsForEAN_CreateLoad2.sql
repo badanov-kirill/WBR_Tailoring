ccbab50c17c69e042130d30d0db0e83286857690
Баданов Кирилл <kirill.badanov@bk.ru>
2023-04-26 22:24:15 +0300

    fox

--- WBR_Tailoring/Tailoring/Synchro/Stored Procedures/ProductsForEAN_CreateLoad2.sql
+++ WBR_Tailoring/Tailoring/Synchro/Stored Procedures/ProductsForEAN_CreateLoad2.sql
@@ -1,6 +1,7 @@
 ﻿CREATE PROCEDURE [Synchro].[ProductsForEAN_CreateLoad2]
 	@pants_id INT,
-	@ean VARCHAR(13)
+	@ean VARCHAR(13),
+	@fabricator_id INT = 1
 AS
 	SET NOCOUNT ON
 	SET XACT_ABORT ON
@@ -13,9 +14,9 @@ AS
 		;
 		MERGE Manufactory.EANCode t
 		USING (
-		      	SELECT	@pants_id pants_id
+		      	SELECT	@pants_id pants_id, @fabricator_id fabricator_id
 		      ) s
-				ON s.pants_id = t.pants_id
+				ON s.pants_id = t.pants_id AND t.fabricator_id = s.fabricator_id
 		WHEN MATCHED THEN 
 		     UPDATE	
 		     SET 	ean = @ean
@@ -23,12 +24,14 @@ AS
 		     INSERT
 		     	(
 		     		pants_id,
-		     		ean
+		     		ean,
+		     		fabricator_id
 		     	)
 		     VALUES
 		     	(
 		     		s.pants_id,
-		     		@ean
+		     		@ean,
+		     		@fabricator_id
 		     	);
 		
 		UPDATE	Synchro.ProductsForEAN2
