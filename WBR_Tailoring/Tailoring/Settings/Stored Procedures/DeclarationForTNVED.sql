e2a8b9c06dcb91dd7d288863352a2c1de943a546
vasilisa <vasilisa@VAS1C>
2023-10-15 22:12:57 +0300

    upd2

--- WBR_Tailoring/Tailoring/Settings/Stored Procedures/DeclarationForTNVED.sql
+++ WBR_Tailoring/Tailoring/Settings/Stored Procedures/DeclarationForTNVED.sql
@@ -1,10 +1,11 @@
-﻿CREATE PROCEDURE Settings.DeclarationForTNVED
+﻿
+CREATE PROCEDURE [Settings].[DeclarationForTNVED]
 @subject_id INT,
 @ct_id INT,
 @consist_type_id INT 
 
 AS
-select sd.declaration_id, sd.declaration_number + ' c: '+ cast(sd.end_date as char) +' по:' + cast(sd.end_date as char) as declarationFull
+select sd.declaration_id, sd.declaration_number + ' c: '+ cast(sd.end_date as char(10)) +'  по: ' + cast(sd.end_date as char(10)) as declarationFull
 from  Products.TNVED_Settigs tnvds
 	INNER JOIN Products.TNVED t
 		ON	t.tnved_id = tnvds.tnved_id 
